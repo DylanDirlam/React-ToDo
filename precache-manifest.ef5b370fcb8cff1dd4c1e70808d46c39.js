43ba9e543f432063935cca5acf58fcf2260045a7
Dylan Dirlam <dadirlam@hotmail.com>
2020-07-28 16:45:24 -0400

    Updates

---
+++ precache-manifest.ef5b370fcb8cff1dd4c1e70808d46c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae8b9a5976b92d53ea856267c1e26d0",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "747ba497a8f7da8af2e4",
+    "url": "/React-ToDo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fc0a09caa8d34c28f5d0",
+    "url": "/React-ToDo/static/js/2.f50a11c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo/static/js/2.f50a11c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747ba497a8f7da8af2e4",
+    "url": "/React-ToDo/static/js/main.3f5dbe5e.chunk.js"
+  },
+  {
+    "revision": "42e537697a4d2967e961",
+    "url": "/React-ToDo/static/js/runtime-main.5b1cba61.js"
+  }
+]);
